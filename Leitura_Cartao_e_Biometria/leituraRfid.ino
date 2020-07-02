fcaf8ad94ab064fd0a6b435777e8d9ea7f95da40
André Oliveira Cunha <54721131+andreocunha@users.noreply.github.com>
2020-07-02 19:10:53 -0300

    Update leituraRfid.ino

--- Leitura_Cartao_e_Biometria/leituraRfid.ino
+++ Leitura_Cartao_e_Biometria/leituraRfid.ino
@@ -15,88 +15,27 @@ void leituraRfid(){
 /***FIM DO BLOCO DE CÓDIGO RESPONSÁVEL POR GERAR A TAG RFID LIDA***/
 
 //-----------------------------------------------------------------------------------------------------------------------------
-
-  if (strID.indexOf("32:80:CD:F2") >= 0){ //Andre
+//Codigos dos cartões RFID
+  if (strID.indexOf("00:00:00:00") >= 0){ 
     permitido();
   }
-  else if(strID.indexOf("01:02:03:04") >= 0 ){//Andre
+  else if(strID.indexOf("00:00:00:00") >= 0 ){
     permitido(); 
   }
 
 
-  else if (strID.indexOf("04:1B:3D:33") >= 0){//Caio
+  else if (strID.indexOf("00:00:00:00") >= 0){
       permitido();
   }
 
   
-  else if (strID.indexOf("86:4A:DC:B0") >= 0){ //Otto
+  else if (strID.indexOf("00:00:00:00") >= 0){ 
       permitido();
   }
 
-  else if (strID.indexOf("36:84:CC:3B") >= 0){ //Rodrigo Laiola
+  else if (strID.indexOf("00:00:00:00") >= 0){
     permitido();
   }
-  
-  else if (strID.indexOf("BE:9A:32:61") >= 0){ //Cartao 1
-    permitido();
-  }
-
-  else if (strID.indexOf("1E:1B:2A:61") >= 0){ //Cartao 2
-    permitido();
-  }
-
-  else if (strID.indexOf("6E:C3:37:61") >= 0){ //Cartao 3
-    permitido();
-  }
-
-  else if (strID.indexOf("DE:11:39:61") >= 0){ //Cartao 4
-    permitido();
-  }
-
-  else if (strID.indexOf("2E:16:3D:61") >= 0){ //Cartao 5
-    permitido();
-  }
-
-  else if (strID.indexOf("1E:62:3C:61") >= 0){ //Cartao 6
-    permitido();
-  }
-
-  else if (strID.indexOf("7E:04:34:61") >= 0){ //Cartao 7
-    permitido();
-  }
-
-  else if (strID.indexOf("6E:43:3D:61") >= 0){ //Cartao 8
-    permitido();
-  }
-
-  else if (strID.indexOf("5E:5E:3C:61") >= 0){ //Cartao 9
-    permitido();
-  }
-
-  else if (strID.indexOf("8E:1D:3D:61") >= 0){ //Cartao 10
-    permitido();
-  }
-
-  else if (strID.indexOf("CE:E8:3A:61") >= 0){ //Cartao 11
-    permitido();
-  }
-
-  else if (strID.indexOf("AE:D4:34:61") >= 0){ //Cartao 12
-    permitido();
-  }
-
-  else if (strID.indexOf("1E:67:2E:61") >= 0){ //Cartao 13
-    permitido();
-  }
-
-  else if (strID.indexOf("FE:E1:32:61") >= 0){ //Cartao 14
-    permitido();
-  }
-
-  else if (strID.indexOf("8E:C2:3B:61") >= 0){ //Cartao 15
-    permitido();
-  }
-    
   else{ //SENÃO, FAZ (CASO A TAG LIDA NÃO SEJÁ VÁLIDA)
     negado();
   }
